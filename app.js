f73b99ecfe3ffa976d1009e55c2821ad772eeb0c
Ruben Casanova <169172216+caasaanoovaa@users.noreply.github.com>
2025-02-19 17:55:09 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+let boton = document.getElementById("add");
+let lista = document.getElementById("list"); 
+let inputTexto = document.getElementById("texto");
+
+function addTask() {
+    if (inputTexto.value !== ""){
+    let nuevoItem = document.createElement("li");
+    nuevoItem.setAttribute("class", "my-3");
+    nuevoItem.textContent = inputTexto.value;
+    lista.appendChild(nuevoItem);
+    addButtonBorrar(nuevoItem);
+    }
+};
+
+function addButtonBorrar(nuevoItem) {
+    let buttonBorrar = document.createElement("button");
+    buttonBorrar.textContent = "Borrar";
+    buttonBorrar.setAttribute("class", "btn btn-primary float-start mx-3");
+    buttonBorrar.addEventListener("click", function () {
+        alert("Borrando el elemento ")
+        nuevoItem.remove();
+    })
+    nuevoItem.appendChild(buttonBorrar);
+};
+
+boton.addEventListener("click", function () {
+    addTask()
+});
+
+inputTexto.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        addTask();
+    }
+});
+
+
+nuevoItem.addEventListener("click", function (){
+    removeTask()
+})
